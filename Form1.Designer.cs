18fc11a6a1c45b3870dcb2492957b56feaa68ea6
YusufGURDAL <yusufgrdl07@gmail.com>
2022-11-06 22:48:20 +0300

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿
+using System;
+
+namespace RTX_Texture_Editor_for_Minecraft
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Slider = new System.Windows.Forms.PictureBox();
+            this.fileOpen = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFile = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.panelCanvas = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Slider)).BeginInit();
+            this.panelCanvas.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Slider
+            // 
+            this.Slider.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.Slider.Location = new System.Drawing.Point(787, 43);
+            this.Slider.Name = "Slider";
+            this.Slider.Size = new System.Drawing.Size(30, 450);
+            this.Slider.TabIndex = 0;
+            this.Slider.TabStop = false;
+            this.Slider.Paint += new System.Windows.Forms.PaintEventHandler(this.Slider_Paint);
+            this.Slider.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Slider_MouseDown);
+            this.Slider.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Slider_MouseMove);
+            this.Slider.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Slider_MouseUp);
+            // 
+            // fileOpen
+            // 
+            this.fileOpen.Location = new System.Drawing.Point(13, 619);
+            this.fileOpen.Name = "fileOpen";
+            this.fileOpen.Size = new System.Drawing.Size(209, 40);
+            this.fileOpen.TabIndex = 2;
+            this.fileOpen.Text = "Dosya aç";
+            this.fileOpen.UseVisualStyleBackColor = true;
+            this.fileOpen.Click += new System.EventHandler(this.fileOpen_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // saveFile
+            // 
+            this.saveFile.Location = new System.Drawing.Point(229, 619);
+            this.saveFile.Name = "saveFile";
+            this.saveFile.Size = new System.Drawing.Size(186, 40);
+            this.saveFile.TabIndex = 3;
+            this.saveFile.Text = "Kaydet";
+            this.saveFile.UseVisualStyleBackColor = true;
+            this.saveFile.Click += new System.EventHandler(this.saveFile_Click);
+            // 
+            // panelCanvas
+            // 
+            this.panelCanvas.BackColor = System.Drawing.Color.DimGray;
+            this.panelCanvas.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panelCanvas.Controls.Add(this.pictureBox1);
+            this.panelCanvas.Location = new System.Drawing.Point(12, 12);
+            this.panelCanvas.Name = "panelCanvas";
+            this.panelCanvas.Size = new System.Drawing.Size(600, 600);
+            this.panelCanvas.TabIndex = 4;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(200, 200);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(200, 200);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(728, 535);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 25);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(793, 535);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 25);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1179, 749);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panelCanvas);
+            this.Controls.Add(this.saveFile);
+            this.Controls.Add(this.fileOpen);
+            this.Controls.Add(this.Slider);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.Slider)).EndInit();
+            this.panelCanvas.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void trackBar1_ValueChanged(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Slider;
+        private System.Windows.Forms.Button fileOpen;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button saveFile;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Panel panelCanvas;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+
+namespace RTX_Texture_Editor_for_Minecraft
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Slider.Width = 30;
+            this.panelCanvas.MouseWheel += PanelCanvas_MouseWheel;
+        }
+        int pixelX, pixelY;
+        float def_value = 0.0f, Min = 0.0f, Max = 1.0f;
+        bool firstTime = true;
+        public float Bar(float value)
+        {
+            return (Slider.Height) * (value - Min) / (float)(Max - Min);
+        }
+        public void thumb(float value)
+        {
+            if (value < Min)
+                value = Min;
+            if (value > Max)
+                value = Max;
+            def_value = value;
+            Slider.Refresh();
+        }
+        public float Slider_height(int y)
+        {
+            return Min + (Max - Min) * y / (float)(Slider.Height);
+        }
+        private void Slider_Paint(object sender, PaintEventArgs e)
+        {
+            float bar_size = 0.45f;
+            float y = 450-Bar(def_value);
+            int x = (int)(Slider.Width * bar_size);
+            if (y > 427)
+                y = 427;
+            e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+            e.Graphics.FillRectangle(Brushes.DimGray, 12, 0, 6, Slider.Height);
+            e.Graphics.FillRectangle(Brushes.Red, 12, y , Slider.Width-2*x, 450 );
+
+           using (Pen pen = new Pen(Color.Black, 8))
+            {
+                e.Graphics.DrawEllipse(pen, x - 6, y +4, Slider.Width / 2, Slider.Width / 2);
+                e.Graphics.FillEllipse(Brushes.Red, x - 6, y+4 , Slider.Width / 2, Slider.Width / 2);
+            }
+            using (Pen pen = new Pen(Color.White, 5))
+            {
+                e.Graphics.DrawEllipse(pen, x -6, y +4, Slider.Width / 2, Slider.Width / 2);
+            }
+
+        }
+        bool mouse = false;
+        private void Slider_MouseDown(object sender, MouseEventArgs e)
+        {
+            mouse = true;
+            thumb(Slider_height(450-e.Y));
+
+        }
+        private void Slider_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!mouse)
+                return;
+            thumb(Slider_height(450-e.Y));
+        }
+        private void Slider_MouseUp(object sender, MouseEventArgs e)
+        {
+            mouse = false;
+        }
+        Image file;
+        Boolean opened = false;
+        void openImage()
+        {
+            DialogResult dr = openFileDialog1.ShowDialog();
+            if (dr == DialogResult.OK)
+            {
+                file = Image.FromFile(openFileDialog1.FileName);
+                opened = true;
+                pictureBox1.Image = file;
+                pictureBox1.Height = file.Height;
+                pictureBox1.Width = file.Width;
+                x = pictureBox1.Width;
+                y = pictureBox1.Height;
+                pictureBox1.Location = new Point((panelCanvas.Width - x) / 2, (panelCanvas.Height - y) / 2);
+                bm = new Bitmap(file);
+                bm_size = bm.Size;
+                zoom = 1;
+                limit = 32;
+                pixelX = x;
+                pixelY = y;
+                int[,] pixel = new int[pixelX, pixelY];
+                
+            }
+        }
+        void saveImage()
+        {
+            if (opened)
+            {
+                SaveFileDialog sfd = new SaveFileDialog();
+                sfd.Filter = "Images|*.png";
+                ImageFormat format = ImageFormat.Png;
+            }
+            else { MessageBox.Show("No image loaded, first upload image "); }
+        }
+        private void saveFile_Click(object sender, EventArgs e)
+        {
+            saveImage();
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (pictureBox1.Image != null)
+                pictureBox1.Dispose();
+        }
+        private void fileOpen_Click(object sender, EventArgs e)
+        {
+            openImage();
+        }
+        Bitmap bm;
+        Size bm_size;
+        int zoom, limit, x, y, factor = 1;
+        private void PanelCanvas_MouseWheel(object sender, MouseEventArgs e)
+        {
+            if (bm != null)
+            {
+                if (firstTime)
+                {
+                    pictureBox1.Image = null;
+                    firstTime = false;
+                }
+                if (e.Delta > 0 && zoom * 2 <= limit)
+                {
+                    zoom = zoom * 2;
+                    factor++;
+                }
+                else if (e.Delta < 0 && zoom / 2 > 0.5f)
+                {
+                    zoom = zoom / 2;
+                    factor--;
+                }
+                pictureBox1.BorderStyle = BorderStyle.None;
+                pictureBox1.Size = new Size(zoom * x, zoom * y);
+                pictureBox1.Location = new Point((panelCanvas.Size.Width - pictureBox1.Width) / 2, (panelCanvas.Size.Height - pictureBox1.Height) / 2);
+                pictureBox1.Invalidate();
+            }
+        }
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if ( opened)
+            {
+                cursorMoving = true;
+                cursorX = e.X;
+                cursorY = e.Y;
+                int xAxis = (int)(cursorX * zoom*(float)Math.Pow(2,5-factor) / (float)(bm.Width*Math.Pow(2,factor-1)));
+                int yAxis = (int)(cursorY * zoom * (float)Math.Pow(2, 5 - factor) / (float)(bm.Height * Math.Pow(2, factor - 1)));
+                label1.Text = xAxis.ToString("f");
+                label2.Text = factor.ToString("d");
+                Point touch = new Point(xAxis*zoom, yAxis*zoom);
+                graphics.FillRectangle(Brushes.Black, touch.X, touch.Y, file.Width * zoom / bm.Width, file.Height * zoom / bm.Height);
+            }
+        }
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (cursorX != -1 && cursorY != -1 && cursorMoving && opened) 
+            {
+                cursorX = e.X;
+                cursorY = e.Y;
+                int xAxis = (int)(cursorX * zoom * (float)Math.Pow(2, 5 - factor) / (float)(bm.Width * Math.Pow(2, factor - 1)));
+                int yAxis = (int)(cursorY * zoom * (float)Math.Pow(2, 5 - factor) / (float)(bm.Height * Math.Pow(2, factor - 1)));
+                Point touch = new Point(xAxis * zoom, yAxis * zoom);
+                graphics.FillRectangle(Brushes.Black, touch.X, touch.Y, file.Width * zoom / bm.Width, file.Height * zoom / bm.Height);
+            }
+        }
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            cursorMoving = false;
+            cursorX = -1;
+            cursorY = -1;
+        }
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            if (file != null)
+            {
+                e.Graphics.InterpolationMode = InterpolationMode.NearestNeighbor;
+                e.Graphics.PixelOffsetMode = PixelOffsetMode.Half;
+                e.Graphics.DrawImage(bm, 0, 0, pictureBox1.Width, pictureBox1.Height);
+                cursorPen = new Pen(Color.Black, file.Width * zoom/bm.Width);
+                cursorPen.StartCap = LineCap.Square;
+                cursorPen.EndCap = LineCap.Square;
+                graphics = pictureBox1.CreateGraphics();
+            }
+        }
+        Graphics graphics;
+        Boolean cursorMoving = false;
+        Pen cursorPen;
+        int cursorX = -1;
+        int cursorY = -1;
+    }
+}
